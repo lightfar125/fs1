de6602334ef31cd3cb0096fa100fd58885988e43
lightfar125 <57426652+lightfar125@users.noreply.github.com>
2025-04-03 14:51:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-name = "fs1"
-compatibility_date = "2025-04-03"
+export default {
+  async fetch(request) {
+    return new Response("Worker is running!", { status: 200 });
+  }
+};
 
-[build]
-command = "npm install && npm run build"
-upload.format = "modules"
-main = "index.js"
